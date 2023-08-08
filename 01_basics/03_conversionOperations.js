183a355ff76bfbc8b18610210f62595f1af5c8e5
ravigrover26 <53804515+ravigrover26@users.noreply.github.com>
2023-08-08 11:20:59 +0000

    changed 03

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,31 @@
+// ************** Operations *******************
+
+// let str1 = "hello"
+// let str2 = " world"
+
+// let str3 =   "30" + 100 + 200;
+// console.log(str3);
+
+// let x = 100;
+// let a = x++;
+// //x++;
+
+// console.log(a,x);
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// ************** Conversion *******************
+
 // let score;
 
 // console.log(score,typeof score);
