8ef383ff313bcdad30d34242a8c147c187bbd1b1
ravigrover26 <53804515+ravigrover26@users.noreply.github.com>
2023-08-08 10:20:34 +0000

    Added 01 and 02 files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 14453
+
+let accountEmail = "ravigrover@test.com"
+
+var accountPassword = "12345"
+
+accountCity = "jaipur"
+var accountState;
+
+// accountId = 2
+
+accountEmail = "rg@rg.com"
+accountPassword = "100100"
+accountCity = "Bihar"
+
+accountPassword = "67890"
+
+// console.log(accountId);
+
+if (true){
+    var accountPassword;
+    console.log('in if scope');
+    console.log(accountPassword);
+}
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// alert(10 + 2);
+
+console.log( 3 + 4);
+
+console.log("Ravi Grover");
