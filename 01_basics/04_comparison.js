7ff582c0173cc136d6febca3be27e4c0d3d8625b
ravigrover26 <53804515+ravigrover26@users.noreply.github.com>
2023-08-09 02:51:57 +0000

    added data types summary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null <= 0); >= or <= makes null to 0 hence it is true
+// console.log(null >= 0);
+// console.log(null === 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined === 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// console.log(2 == 2);
+// console.log("2" == 2);
+// console.log( 2 === 2);
+// console.log("2" === 2);
+
+// console.log("banana" > "apple");
+// console.log(100 > 50);
---
+++ 01_basics/05_datatypesummary.js
@@ -0,0 +1,54 @@
+// let score = 100;
+
+// let age = 23;
+
+// let interest = 5.234;
+
+// let gameName = "NFS"
+
+// let isWinner = false;
+
+// let isLoser = null;
+
+// let isLogged;
+
+
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// console.log(id, typeof id);
+// console.log(anotherId, typeof anotherId);
+
+
+
+// const bigNum = 12345678901234567890n
+// console.log(bigNum, typeof bigNum);
+
+
+// const cars = ["camry","corolla","mustang"]
+
+// console.log(cars , typeof cars);
+
+// const myCar = {
+//     make:"Toyota",
+//     model:"Kluger",
+//     fuel:"Hybrid",
+//     engine:2000
+// }
+
+// console.log(myCar,typeof myCar);
+
+
+
+// const greet = function(){
+//     console.log("Good Morning!");
+// }
+
+// console.log(greet,typeof greet);
+
+
+
+
+
